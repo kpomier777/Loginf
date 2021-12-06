3e86d3b1513bf67a0bb245c200dad940a6741d9a
kpomier777 <kevinpomier.kp@gmail.com>
2021-12-05 21:10:11 -0800

    4 files modified, 1 added hoy

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  username='';
+  password='';
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit():void {
+  }
+  onLogin():void{
+    console.log('USERNAME',this.username);
+    console.log('PASSWORD',this.password);
   }
 
 }
